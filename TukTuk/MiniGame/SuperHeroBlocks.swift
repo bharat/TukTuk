1bd05105e70570b287683dc48b5f8fed0e7a116c
Bharat Mediratta <bharat@menalto.com>
2018-05-26 10:32:14 -0700

    refactor more functionality directly into the Scene

--- TukTuk/MiniGame/SuperHeroBlocks.swift
+++ TukTuk/MiniGame/SuperHeroBlocks.swift
@@ -1,5 +1,5 @@
- //
-//  Blocks.swift
+//
+//  SuperHeroBlocks.swift
 //  TukTuk
 //
 //  Created by Bharat Mediratta on 5/14/18.
@@ -11,13 +11,36 @@ import UIKit
 import SceneKit
 
  class SuperHeroBlocks: MiniGame {
-    static var title = "Super-hero blocks!"
+    static var title = "Super-Hero blocks!"
 
     required init() {
     }
 
     func vc() -> UIViewController {
         return SuperHeroBlocksUIViewController()
+    }
+    
+    class SuperHeroBlocksUIViewController: UIViewController {
+        var sceneView: SCNView!
+        
+        override func viewDidAppear(_ animated: Bool) {
+            let effect = UIBlurEffect(style: .light)
+            let effectView = UIVisualEffectView(effect: effect)
+            effectView.frame = view.frame
+            view.addSubview(effectView)
+            
+            let scene = Scene()
+            sceneView = SCNView(frame: view.frame)
+            sceneView.backgroundColor = UIColor.lightGray
+            sceneView.autoenablesDefaultLighting = true
+            sceneView.allowsCameraControl = false
+            sceneView.scene = scene
+            sceneView.gestureRecognizers = [
+                UITapGestureRecognizer(target: scene, action: #selector(scene.tapHeroBlock(gesture:)))
+            ]
+            
+            effectView.contentView.addSubview(sceneView)
+        }
     }
 
     enum Hero: Int {
@@ -56,35 +79,46 @@ import SceneKit
         }
     }
     
-    class SuperHeroBlocksUIViewController: UIViewController {
+    class Scene: SCNScene {
         var heroBlocks: [SCNNode] = []
-        var sceneView: SCNView!
 
-        override func viewDidAppear(_ animated: Bool) {
-            let effect = UIBlurEffect(style: .light)
-            let effectView = UIVisualEffectView(effect: effect)
-            effectView.frame = view.frame
-            view.addSubview(effectView)
+        override init() {
+            super.init()
+            
+            let camera = SCNNode()
+            camera.camera = SCNCamera()
+            camera.position = SCNVector3(x: 5, y: -50, z: 125)
+            camera.runAction(SCNAction.move(to: SCNVector3(x: 0, y: 0, z: 100), duration: 2.0))
+            rootNode.addChildNode(camera)
 
-            let scene = Scene()
-            heroBlocks = scene.addHeroBlocks()
-
-            sceneView = SCNView(frame: view.frame)
-            sceneView.backgroundColor = UIColor.lightGray
-            sceneView.autoenablesDefaultLighting = true
-            sceneView.allowsCameraControl = false
-            sceneView.scene = scene
-            sceneView.gestureRecognizers = [
-                UITapGestureRecognizer(target: self, action: #selector(tapHeroBlock(gesture:)))
-            ]
-
-            effectView.contentView.addSubview(sceneView)
+            let box = SCNBox(width: 10.0, height: 10.0, length: 10.0, chamferRadius: 1)
+            
+            box.materials = Hero.all.map {
+                let material = SCNMaterial()
+                material.diffuse.contents = $0.image
+                return material
+            }
+            
+            for yLoc in ([-3.0, -1.0, 1.0, 3.0].map { $0 * Float(box.height) }) {
+                let hero = Block(geometry: box)
+                hero.position = SCNVector3(x: 0, y: yLoc, z: 20)
+                rootNode.addChildNode(hero)
+                heroBlocks.append(hero)
+                
+                hero.show(face: Hero.all.random!.rawValue)
+                hero.wiggle()
+            }
         }
-
+        
+        required init(coder: NSCoder) {
+            fatalError("Not yet implemented")
+        }
+        
         @objc func tapHeroBlock(gesture: UITapGestureRecognizer) {
+            let sceneView = gesture.view as! SCNView
             let point = gesture.location(in: sceneView)
             let hits = sceneView.hitTest(point, options: nil)
-
+            
             if let block = hits.first?.node as? Block {
                 block.stopWiggling()
                 block.showNextFace()
@@ -125,45 +159,6 @@ import SceneKit
                     SCNAction.rotateBy(x: 0, y: 0, z:  0.16, duration: 0.050),
                     SCNAction.rotateBy(x: 0, y: 0, z: -0.08, duration: 0.025),
                 ])))
-        }
-    }
-
-    class Scene: SCNScene {
-        override init() {
-            super.init()
-
-            let camera = SCNNode()
-            camera.camera = SCNCamera()
-            camera.position = SCNVector3(x: 5, y: -50, z: 125)
-            camera.runAction(SCNAction.move(to: SCNVector3(x: 0, y: 0, z: 100), duration: 2.0))
-
-            rootNode.addChildNode(camera)
-        }
-
-        required init(coder: NSCoder) {
-            fatalError("Not yet implemented")
-        }
-
-        func addHeroBlocks() -> [Block] {
-            let box = SCNBox(width: 10.0, height: 10.0, length: 10.0, chamferRadius: 1)
-
-            box.materials = Hero.all.map {
-                let material = SCNMaterial()
-                material.diffuse.contents = $0.image
-                return material
-            }
-
-            var heroes: [Block] = []
-            for yLoc in ([-3.0, -1.0, 1.0, 3.0].map { $0 * Float(box.height) }) {
-                let hero = Block(geometry: box)
-                hero.position = SCNVector3(x: 0, y: yLoc, z: 20)
-                rootNode.addChildNode(hero)
-                heroes.append(hero)
-                
-                hero.show(face: Hero.all.random!.rawValue)
-                hero.wiggle()
-            }
-            return heroes
         }
     }
 }
